2202adbec02ce21b291c1f205495e521c371187a
nikkinine9 <nikkinine75@gmail.com>
2021-02-23 21:47:40 -0700

    created back end

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+    "mongodb+srv://innotech:Jj5jG6@cluster0.yhgrx.mongodb.net/Blog_post?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
