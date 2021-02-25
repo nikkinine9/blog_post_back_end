cd974f7c14eff9fb43955504964e48f9f239b15e
nikkinine9 <nikkinine75@gmail.com>
2021-02-24 19:39:27 -0700

    created new functions

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import { Mongoose } from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async(req, res) => {
@@ -19,4 +20,15 @@ export const createPost = async(req, res) => {
         res.status(409).json({ message: error.message })
     }
     res.send('Post Creation');
+}
+
+// /posts/
+// 123
+
+export const updatePost = async(req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    res.json(updatedPost);
 }
